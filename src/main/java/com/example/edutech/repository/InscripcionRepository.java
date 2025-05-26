9564741eb83315cb4fbdef8342ebd5dfaa315dff
osvaldoq1800 <osv.quiroga@duocuc.cl>
2025-05-26 00:40:56 -0400

    repository

--- src/main/java/com/example/edutech/repository/InscripcionRepository.java
+++ src/main/java/com/example/edutech/repository/InscripcionRepository.java
@@ -1,5 +1,14 @@
 package com.example.edutech.repository;
 
-public class InscripcionRepository {
+import com.example.edutech.model.Curso;
+import com.example.edutech.model.Inscripcion;
+import com.example.edutech.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
+    List<Inscripcion> findByUsuario(Usuario usuario);
+    List<Inscripcion> findByCurso(Curso curso);
+    boolean existsByUsuarioAndCurso(Usuario usuario, Curso curso);
 }
