7d9228fbe68a7ec27c4b09b934406c51bf246507
osvaldoq1800 <osv.quiroga@duocuc.cl>
2025-05-26 00:06:18 -0400

    arreglo

--- src/main/java/com/example/edutech/model/Usuario.java
+++ src/main/java/com/example/edutech/model/Usuario.java
@@ -3,6 +3,7 @@ package com.example.edutech.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
